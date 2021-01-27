0a7cdfeb14290ec700d85a46c71ae7efcc256f15
EIndriksons <edgars@indriksons.lv>
2021-01-27 19:23:34 +0200

    Event listeners

--- src/bot.js
+++ src/bot.js
@@ -3,4 +3,12 @@ const { Client, Message } = require('discord.js');
 
 const client = new Client();
 
+client.on('ready', () => {
+  console.log('kurwa');
+});
+
+client.on('message', (message) => {
+  console.log(message.content);
+});
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
