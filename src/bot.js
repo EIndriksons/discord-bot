f15410b31f1e96040c284c9bdbe00b9ebce98df4
EIndriksons <edgars@indriksons.lv>
2021-01-27 23:39:25 +0200

    Reading and responding to messages

--- src/bot.js
+++ src/bot.js
@@ -2,13 +2,18 @@ require('dotenv').config();
 const { Client, Message } = require('discord.js');
 
 const client = new Client();
-
-client.on('ready', () => {
-  console.log('kurwa');
-});
+const PREFIX = '!';
 
 client.on('message', (message) => {
-  console.log(message.content);
+  if (message.author.bot) return;
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
+
+    if (CMD_NAME === 'help') {
+      message.channel.send('Help');
+    } else if (CMD_NAME === 'stock') {
+    }
+  }
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
