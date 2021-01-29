d7dc51e5507fc747a5ca0d9afeb46da79ad2e125
EIndriksons <edgars@indriksons.lv>
2021-01-29 22:38:34 +0200

    Iex API

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const { Client, Message } = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
+const iex = require('iexcloud_api_wrapper');
 
 const client = new Client();
 const PREFIX = '!';
@@ -10,8 +11,20 @@ client.on('message', (message) => {
     const [CMD_NAME, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
 
     if (CMD_NAME === 'help') {
-      message.channel.send('Help');
+      message.channel.send('Apēd skrūves dauni!');
     } else if (CMD_NAME === 'stock') {
+      iex.quote(args[0]).then((data) => {
+        const embed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(`${data.symbol}, ${data.companyName}`)
+          .addFields({ name: 'Price', value: `${data.latestPrice}`, inline: false });
+
+        message.channel.send(embed);
+      });
+
+      iex.history(args[0], { date: '2021-01-27' }).then((data) => {
+        console.log(data);
+      });
     }
   }
 });
