77c538389fc90a0a7be0799c11e90279b8f2fa99
EIndriksons <edgars@indriksons.lv>
2021-01-30 02:54:22 +0200

    Added additional API and Roll functionality

--- src/bot.js
+++ src/bot.js
@@ -8,25 +8,42 @@ const PREFIX = '!';
 client.on('message', (message) => {
   if (message.author.bot) return;
   if (message.content.startsWith(PREFIX)) {
+    // Message parser function
     const [CMD_NAME, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
 
+    // Command Switch
     if (CMD_NAME === 'help') {
+      // HELP
       message.channel.send('Apēd skrūves dauni!');
     } else if (CMD_NAME === 'stock') {
+      // STOCK {SYMBOL}
       iex.quote(args[0]).then((data) => {
         const embed = new MessageEmbed()
           .setColor('#0099ff')
           .setTitle(`${data.symbol}, ${data.companyName}`)
           .addFields({ name: 'Price', value: `${data.latestPrice}`, inline: false });
 
+        console.log(data);
         message.channel.send(embed);
       });
 
-      iex.history(args[0], { date: '2021-01-27' }).then((data) => {
-        console.log(data);
-      });
+      iex
+        .ohlc(args[0])
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (CMD_NAME === 'roll') {
+      // ROLL
+      message.channel.send(getRandomInt(7));
     }
   }
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
