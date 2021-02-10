c65456c4acf216a0c6e4847c9800903008adc2ed
EIndriksons <edgars@indriksons.lv>
2021-02-10 12:08:36 +0200

    Introduced helper.js file

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const axios = require('axios');
 const { Client, MessageEmbed } = require('discord.js');
 
-const client = new Client();
+const { nFormatter, shuffle } = require('./helpers');
+
+const client = new Client({ partials: ['MESSAGE'] });
 const PREFIX = '!';
 
 client.on('message', (message) => {
@@ -61,47 +63,3 @@ client.on('message', (message) => {
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function nFormatter(num, digits) {
-  var si = [
-    { value: 1, symbol: '' },
-    { value: 1e3, symbol: ' k' },
-    { value: 1e6, symbol: ' M' },
-    { value: 1e9, symbol: ' B' },
-    { value: 1e12, symbol: ' T' },
-    { value: 1e15, symbol: ' P' },
-    { value: 1e18, symbol: ' E' },
-  ];
-  var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-  var i;
-  for (i = si.length - 1; i > 0; i--) {
-    if (num >= si[i].value) {
-      break;
-    }
-  }
-  return (num / si[i].value).toFixed(digits).replace(rx, '$1') + si[i].symbol;
-}
-
-function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
